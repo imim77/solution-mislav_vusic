416825f111873a2e6eedf7a4614641a0df894194
imim77 <mislav.vusic@gmail.com>
2026-07-03 09:19:27 +0200

    feat(frontend) Add inital ProductCard component

---
+++ frontend/src/components/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductCard() {
+  return (
+    <div>Pozz iz product card</div>
+  )
+}
+
+export default ProductCard
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,15 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5188',
+                rewrite: (path) => path.replace(/^\/api/, ''),
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    },
+
 })

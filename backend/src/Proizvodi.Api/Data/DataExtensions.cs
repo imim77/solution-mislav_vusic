0d42adc7f58fcf657f27eb41fe9aa9754a374a5b
imim77 <mislav.vusic@gmail.com>
2026-07-05 21:24:18 +0200

    refactor(backend) rename MigrateDd to MigrateDatabase

--- backend/src/Proizvodi.Api/Data/DataExtensions.cs
+++ backend/src/Proizvodi.Api/Data/DataExtensions.cs
@@ -4,10 +4,16 @@ namespace Proizvodi.Api.Data;
 
 public static class DataExtensions
 {
-    public static void MigrateDd(this WebApplication app)
+    public static void MigrateDatabase(this WebApplication app)
     {
         using var scope = app.Services.CreateAsyncScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<ProizvodiContext>();
         dbContext.Database.Migrate();
+    }
+
+    [Obsolete("Use MigrateDatabase instead.")]
+    public static void MigrateDd(this WebApplication app)
+    {
+        app.MigrateDatabase();
     }
 }
